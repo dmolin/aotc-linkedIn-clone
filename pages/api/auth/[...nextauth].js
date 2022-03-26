c1898906b8a44f6b31c031010161881e7430c05b
dmolin <davide.molin@gmail.com>
2022-03-26 11:15:48 +0000

    configuring a DB adapter for Mongo

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,23 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+
+import clientPromise from "@/server/mongodb";
 
 export default NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
-  ]
+  ],
+  pages: {
+    signIn: "/home"
+  },
+  session: {
+    strategy: "database"
+  },
+  debug: true
 });
