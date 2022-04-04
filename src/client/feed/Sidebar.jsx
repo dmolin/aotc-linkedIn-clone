21ebb733ae39dae7a2da4152e09e9f381f38d8be
dmolin <davide.molin@gmail.com>
2022-04-04 08:05:08 +0100

    Working on the social panels in the sidebar

--- src/client/feed/Sidebar.jsx
+++ src/client/feed/Sidebar.jsx
@@ -16,12 +16,12 @@ const Sidebar = props => {
         <h4 className="leading-4 text-dimmed font-bold dark:text-white/50">{name}</h4>
         {extra || null}
       </div>
-      <span className="text-blue-400 font-bold">{value}</span>
+      <span className="text-blue-600 font-bold">{value}</span>
     </li>
   );
 
   return (
-    <div className="flex flex-col gap-2 min-w-max 2lg:w-[250px]">
+    <div className="flex flex-col gap-2 min-w-max 2lg:w-[250px] text-sm">
       {/* top */}
       <div className="li-panel">
         {/* profile */}
@@ -39,18 +39,18 @@ const Sidebar = props => {
             <h4 className="mt-14 font-bold text-lg decoration-purple-700">{data?.user?.name}</h4>
           </div>
           <div className="px-4">
-            <p className="text-dimmed text-sm">{data?.user?.email}</p>
+            <p className="text-dimmed">{data?.user?.email}</p>
           </div>
         </div>
         {/* stats */}
         <div className="hidden md:block">
-          <div className="li-border py-4 text-sm">
+          <div className="li-border py-4">
             <ul className="flex flex-col">
               {_renderStat("Who viewed your profile", 321)}
               {_renderStat("Connections", 1290, <a className="font-bold pt-2">Manage your network</a>)}
             </ul>
           </div>
-          <div className="flex li-border li-selectable li-clickable flex-col gap-0 py-4 text-sm px-4">
+          <div className="flex li-border li-selectable li-clickable flex-col gap-0 py-4 px-4">
             <span className="text-dimmed text-sm">Access exclusive tools & insights</span>
             <div>
               <span className="w-3 h-3 bg-gradient-to-tr from-yellow-700 to-yellow-200 inline-block rounded-sm mr-1" />
@@ -59,7 +59,7 @@ const Sidebar = props => {
           </div>
           <div className="flex flex-row gap-2 li-selectable li-clickable li-border px-4 py-2 text-gray-800 dark:text-white/75">
             <HiBookmark className="text-xl -mx-1"/>
-            <span className="font-bold text-sm">My items</span>
+            <span className="font-bold">My items</span>
           </div>
         </div>
       </div>
--- src/client/feed/SocialPanel.jsx
+++ src/client/feed/SocialPanel.jsx
@@ -1,8 +1,40 @@
+import { Button } from "@blueprintjs/core";
+import { HiChevronDown, HiPlusSm } from "react-icons/hi";
+
 const SocialPanel = props => {
   const { className = "" } = props;
+
+  const _renderLink = (name, icon, defOptions) => {
+    const options = {
+      link: true,
+      ...defOptions
+    };
+    return (
+      <div className="flex flex-row justify-between items-center group">
+        {options?.link ?
+          <a href="#" className="flex-1 li-link font-bold"><h4>{name}</h4></a> :
+          <h4>{name}</h4>
+        }
+        {icon ?
+          <Button
+            minimal
+            icon={icon}
+            className={`rounded-full dark:text-white/75 dark:hover:text-white/75 ${options.persistent ? "" : "invisible group-hover:visible"}`}
+          /> :
+          <Button minimal icon="circle" className="invisible" />
+        }
+      </div>
+    );
+  };
+
   return (
     <div className={`li-panel ${className}`}>
-
+      <div className="flex flex-col gap-3 p-4">
+        {_renderLink("Recent", <HiChevronDown />, { link: false })}
+        {_renderLink("Groups", <HiChevronDown />)}
+        {_renderLink("Events", <HiPlusSm />, { persistent: true })}
+        {_renderLink("Followed Hashtags")}
+      </div>
     </div>
   );
 };
